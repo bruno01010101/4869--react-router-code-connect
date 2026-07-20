13d4579d28f9828dc4e8474f2930b4f8c06079f3
Bruno dos santos <bruno16082006@gmail.com>
2026-07-20 16:19:57 -0300

    primeiro commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,16 @@ import { Login } from './pages/Login/index.jsx'
 import { Register } from './pages/Register/index.jsx'
 import { Feed } from './pages/Feed/index.jsx'
 import { BlogPost } from './pages/BlogPost/index.jsx'
+import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter>
     <Login />
-    {/* <Register /> */}
-    {/* <Feed /> */}
-    {/* <BlogPost /> */}
+      {/* <Register /> */}
+      {/* <Feed /> */}
+      {/* <BlogPost /> */}
+    </BrowserRouter>
+    
   </StrictMode>,
 )
